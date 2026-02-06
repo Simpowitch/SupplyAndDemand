16d779fac0274ca45b1bb1c049ee767ea1a7395f
BEAST\wosss <woss.simon@gmail.com>
2026-02-06 11:00:15 +0100

    Reworked inputmanager to leave responsibility for using results from keypresses to the caller

--- SupplyAndDemand/InputManager.h
+++ SupplyAndDemand/InputManager.h
@@ -1,9 +1,30 @@
 #pragma once
+#include <array>
+#include <limits>
 
-namespace Input
+class InputManager
 {
-	int ReadNumericInput();
-	bool ReadYesNoInput();
-	char ReadCharInput();
-}
+public:
+	void Update();
+	bool WasKeyPressed(const unsigned char key) const
+	{
+		return currentState[key] && !lastState[key];
+	}
+	bool WasKeyReleased(const unsigned char key) const
+	{
+		return !currentState[key] && lastState[key];
+	}
+	bool IsKeyHeld(const unsigned char key) const
+	{
+		return currentState[key];
+	}
+
+private:
+	std::array<bool, std::numeric_limits<unsigned char>::max() + 1> currentState{};
+	std::array<bool, std::numeric_limits<unsigned char>::max() + 1> lastState{};
+};
+
+//int ReadNumericInput();
+//bool ReadYesNoInput();
+//char ReadCharInput();
 
