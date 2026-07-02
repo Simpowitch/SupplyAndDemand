5f4e557d047f9f53954f52f9e1d63d5dd9f50fcf
BEAST\wosss <woss.simon@gmail.com>
2026-07-02 16:38:58 +0200

    Utility to get closest

--- SupplyAndDemand/MathUtils.h
+++ SupplyAndDemand/MathUtils.h
@@ -2,6 +2,9 @@
 
 #include "Float2.h"
 #include "RNG.h"
+#include <math.h>
+#include <vector>
+
 
 Float2 GetRandomFloat2(const int xMax, const int yMax)
 {
@@ -11,4 +14,23 @@ Float2 GetRandomFloat2(const int xMax, const int yMax)
 Float2 GetRandomFloat2(const Float2 max)
 {
 	return Float2(GetRandomInt(0, max.x), GetRandomInt(0, max.y));
+}
+
+int GetClosest(const Float2 origin, const std::vector<Float2>& targets)
+{
+	float bestMagnitude = FLT_MAX;
+	int bestIndex = -1;
+
+	for (size_t i = 0; i < targets.size(); i++)
+	{
+		auto sqrMagnitude = (origin - targets[i]).SqrMagnitude();
+
+		if (sqrMagnitude >= bestMagnitude)
+		{
+			continue;
+		}
+		bestIndex = i;
+		bestMagnitude = sqrMagnitude;
+	}
+	return bestIndex;
 }
